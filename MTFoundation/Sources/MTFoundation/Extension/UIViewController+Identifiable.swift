4bfc7fd0ee38f468ec9c604211be27321ab5d6a1
Murilo Teixeira <murilogteixeira@gmail.com>
2021-06-19 22:57:18 -0300

    Add the identifiable files

---
+++ MTFoundation/Sources/MTFoundation/Extension/UIViewController+Identifiable.swift
@@ -0,0 +1,12 @@
+//
+//  UIViewController+Identifiable.swift
+//  
+//
+//  Created by Murilo Teixeira on 19/06/21.
+//
+
+import UIKit
+
+public extension UIViewController: Identifiable {}
+public extension UICollectionViewCell: Identifiable {}
+public extension UITableViewCell: Identifiable {}
---
+++ MTFoundation/Sources/MTFoundation/Protocol/Identifiable.swift
@@ -0,0 +1,16 @@
+//
+//  Identifiable.swift
+//  
+//
+//  Created by Murilo Teixeira on 19/06/21.
+//
+
+import Foundation
+
+public protocol Identifiable {
+    static var uniqueIdentifier: String { get }
+}
+
+public extension Identifiable {
+    static var uniqueIdentifier: String { String(describing: self) }
+}
