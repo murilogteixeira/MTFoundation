ec6f97f3850ee9242f395511bc670509727be287
Murilo Teixeira <murilogteixeira@gmail.com>
2021-06-29 23:17:36 -0300

    Create tests

--- MTFoundation/Tests/MTFoundationTests/MTFoundationTests.swift
+++ MTFoundation/Tests/MTFoundationTests/MTFoundationTests.swift
@@ -2,14 +2,23 @@
     @testable import MTFoundation
 
     final class MTFoundationTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-//            XCTAssertEqual(MyLibrary.text, "Hello, World!")
+        
+        func testImageLoader() {
             let image = UIImageView()
             image.loadImage(from: URL(string: "https://appicon.co/logo.png")!)
+        }
+        
+        func testUIEdgeInsetsInitializers() {
+            var insets = UIEdgeInsets()
+            XCTAssertEqual(insets.top, 0)
+            XCTAssertEqual(insets.left, 0)
+            XCTAssertEqual(insets.bottom, 0)
+            XCTAssertEqual(insets.right, 0)
             
-            UIEdgeInsets(vertical: 0)
+            insets = UIEdgeInsets(vertical: 10, horizontal: 10)
+            XCTAssertEqual(insets.top, 10)
+            XCTAssertEqual(insets.left, 10)
+            XCTAssertEqual(insets.bottom, 10)
+            XCTAssertEqual(insets.right, 10)
         }
     }
