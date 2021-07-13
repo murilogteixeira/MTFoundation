8e19a93ee05796f5e3fd4033118c0b6e4d2d0814
Murilo Teixeira <murilogteixeira@gmail.com>
2021-07-13 15:51:53 -0300

    Remove dependencies

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-         .package(url: "https://github.com/murilogteixeira/MTAutoLayoutKit", from: "1.0.0"),
+//         .package(url: /* url */, from: /* source */),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
