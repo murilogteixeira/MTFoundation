225b4654febb7ef1b5f190c76dacab8dcaaf6f58
Murilo Teixeira <murilogteixeira@gmail.com>
2021-10-30 15:33:38 -0300

    Create base64 string functinos

--- MTFoundation/Sources/MTFoundation/Extension/String+Extensions.swift
+++ MTFoundation/Sources/MTFoundation/Extension/String+Extensions.swift
@@ -23,5 +23,13 @@ public extension String {
         }
         return 0
     }
-    
+
+    func toBase64() -> String {
+        self.data(using: .utf8)?.base64EncodedString() ?? ""
+    }
+
+    func fromBase64() -> String? {
+        guard let data = Data(base64Encoded: self) else { return nil }
+        return String(data: data, encoding: .utf8)
+    }
 }
